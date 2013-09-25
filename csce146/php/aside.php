d4e3fdfe198da74550aa99257f9ca18fe233cd2c
Heath Carroll <heath@roguishexploits.com>
2013-09-25 16:48:57 -0400

    trying out some php...

---
+++ csce146/php/aside.php
@@ -0,0 +1,33 @@
+		<div id="aside">
+			<h2>Class Info</h2>
+			<h3 style="margin-top: 5px">Lecture</h3>
+			<p>T/Th 6:00p-7:15p SWGN 2A14</p>
+			<h4 style="margin-top: 5px">Heath Carroll</h4>
+			<h4>Instructor</h4>
+			<p>Office Hours: Friday by appt<br>
+			Office: SWGN 1D49<br>Email: CarrollH at email.sc.edu</p>
+			<h4 style="margin-top: 5px">Earron Twitty</h4>
+			<h4>Supplimental Instructor</h4>
+			<p>Instruction Location: HUMCB 301</p>
+			<p>Instruction Hours:<br>
+				<ul style="list-style: none">
+					<li>Sun - 5-6pm</li>
+					<li>Tue - 8-9pm</li>
+					<li>Wed - 8-9pm</li>
+				</ul>
+			</p><br>
+			
+			<h3>Lab Section 004</h3>
+			<p>W 3:30p-5:25p SWGN 1D11</p>
+			<h4 style="margin-top: 5px">Aniqua Baset</h4>
+			<p>Email: Baset at email.sc.edu</p>
+			<h4>Robert Ellis</h4>
+			<p>Email: EllisRL2 at email.sc.edu</p><br>
+			
+			<h3>Lab Section 005</h3>
+			<p>W 5:50p-7:45p SWGN 1D11</p>
+			<h4 style="margin-top: 5px">Jiting Xu</h4>
+			<p>Email: Xu57 at email.sc.edu</p>
+			<h4>Dazhou Guo</h4>
+			<p>Email: Guo2004131 at gmail.com</p><br>
+		</div>
