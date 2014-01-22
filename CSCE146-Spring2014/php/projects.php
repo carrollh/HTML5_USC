afb3bfed99427818b9c83f2797915f1ff8d667f3
unknown <carrollha@.sum.tec.sc.us>
2014-01-22 16:30:08 -0500

    project page added to 146, some tweaking of hierarchy so that php stuff works without duplicating code...

---
+++ CSCE146-Spring2014/php/projects.php
@@ -0,0 +1,25 @@
+<?php include('./header.php');?>
+	<div id="content">
+		<table>
+			<tr><th style="width: 75%">Title</th><th>Due Date</th></tr>
+			<tr><td><a href="../proj/Project1.pdf">Project 1</a></td>
+				<td>11:55p 01/19/14</td></tr>
+			<tr><td><a href="../proj/Project2.html">Project 2</a></td>
+				<td>11:55p 01/26/14</td></tr>
+			<tr><td><a href="../proj/Project3.pdf">Project 3</a></td>
+				<td>TBD</td></tr>
+			<tr><td><a href="../proj/Project4.pdf">Project 4</a></td>
+				<td>TBD</td></tr>
+			<tr><td><a href="../proj/Project5.pdf">Project 5</a></td>
+				<td>TBD</td></tr>
+			<tr><td><a href="../proj/Project6.pdf">Project 6</a></td>
+				<td>TBD</td></tr>
+				<tr><td><a href="./proj/Project7.pdf">Project 7</a></td>
+				<td>TBD</td></tr>
+			<tr><td><a href="../proj/Project8.pdf">Project 8</a></td>
+				<td>TBD</td></tr>
+			<tr><td><a href="../proj/Project9.pdf">Project 9</a></td>
+				<td>TBD</td></tr>
+		</table>
+	</div>
+<?php include('./footer.php');?>
